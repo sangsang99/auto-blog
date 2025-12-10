5ff2851be8b53ec8094e70b53b1e342b5a84d873
sangsang <idolphin2020@gmail.com>
2025-12-10 22:28:35 +0900

    레이아웃 수정, content안보이는거 수정

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,7 +1,42 @@
 /*!
-* Start Bootstrap - Blog Post v5.0.8 (https://startbootstrap.com/template/blog-post)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+ * Start Bootstrap - Blog Post v5.0.8 (https://startbootstrap.com/template/blog-post)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
+ */
+
+// Keep the content pushed below the navbar without hard-coded spacing.
+(() => {
+    const root = document.documentElement;
+    const body = document.body;
+    const navSelector = "body.page > header .navbar";
+
+    const updateHeaderHeight = () => {
+        const nav = document.querySelector(navSelector);
+        if (!nav) return;
+
+        const { height } = nav.getBoundingClientRect();
+        if (height > 0) {
+            const measured = `${Math.ceil(height)}px`;
+            root.style.setProperty("--header-height", measured);
+            body.style.setProperty("--header-height", measured);
+        }
+    };
+
+    const init = () => {
+        updateHeaderHeight();
+        const nav = document.querySelector(navSelector);
+
+        if (nav && typeof ResizeObserver !== "undefined") {
+            const resizeObserver = new ResizeObserver(updateHeaderHeight);
+            resizeObserver.observe(nav);
+        }
+
+        window.addEventListener("resize", updateHeaderHeight, { passive: true });
+    };
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", init);
+    } else {
+        init();
+    }
+})();
