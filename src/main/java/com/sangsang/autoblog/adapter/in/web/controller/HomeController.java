c8bf746fdda793bb591a62315b398043e85c227d
sangsang <idolphin2020@gmail.com>
2025-12-29 22:31:54 +0900

    docs(swg) 최소설정

--- src/main/java/com/sangsang/autoblog/adapter/in/web/controller/HomeController.java
+++ src/main/java/com/sangsang/autoblog/adapter/in/web/controller/HomeController.java
@@ -3,8 +3,15 @@ package com.sangsang.autoblog.adapter.in.web.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Controller
+
+@Tag(name = "Home-Controller", description = "HomePage & Health Check")
 public class HomeController {
 
     @GetMapping("/")
@@ -18,6 +25,9 @@ public class HomeController {
     }
 
     @GetMapping("/healthCheck")
+    @ResponseBody
+    @Operation(summary = "Health Check", description = "서버 상태 확인")
+    @ApiResponse(responseCode = "200", description = "OK")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
     }
---
+++ src/main/java/com/sangsang/autoblog/infra/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.sangsang.autoblog.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+/**
+ * Swagger springdoc-ui 구성 파일
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("AutoBlog API Document")
+                .version("v0.0.1")
+                .description("AutoBlog 프로젝트의 API 명세서입니다.");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+
+}
